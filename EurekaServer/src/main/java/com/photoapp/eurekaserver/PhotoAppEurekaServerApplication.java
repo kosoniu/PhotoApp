7a388512cb3665c901c43e84439b2e4aaeb67bec
NGDK78 <karol.koson@motorolasolutions.com>
2020-09-14 14:42:29 +0200

    eureka and users ms

---
+++ EurekaServer/src/main/java/com/photoapp/eurekaserver/PhotoAppEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.photoapp.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppEurekaServerApplication.class, args);
+    }
+
+}
